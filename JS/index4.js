5ece23b52fe1c5ac618d8affde4a565e9d373990
muzammil-khan710 <khanmuzammil549@gmail.com>
2021-08-10 00:22:12 +0530

    area Quiz completed

---
+++ JS/index4.js
@@ -0,0 +1,16 @@
+const sides = document.querySelectorAll(".sides")
+const areaBtn = document.querySelector("#calculate-area")
+const outputDiv = document.querySelector("#output")
+
+function calculateProductOfSides(b, h) {
+    const productOfSides = b * h;
+    return productOfSides;
+}
+
+function calculateArea() {
+    const productOfSides = calculateProductOfSides(Number(sides[0].value), Number(sides[1].value))
+    const area  = productOfSides * 1/2;
+    outputDiv.innerText = "The area of triangle is" + area;
+}
+
+areaBtn.addEventListener("click", calculateArea)
