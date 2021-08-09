6e99bc618c52fcca71511cef649d7247cc8a30b1
muzammil-khan710 <khanmuzammil549@gmail.com>
2021-08-09 23:52:44 +0530

    hypotenuse Quiz completed

---
+++ JS/index3.js
@@ -0,0 +1,17 @@
+const sides =  document.querySelectorAll(".input")
+const hypotenuseBtn = document.querySelector("#hypotenuse")
+const outputDiv = document.querySelector("#output")
+
+function calculateSquaresOfSides(p, b) {
+    const sumOfSquares = p*p + b*b
+    return sumOfSquares;
+}
+
+function checkHypotenuse() {
+    const sumOfSquares = calculateSquaresOfSides(Number(sides[0].value), Number(sides[1].value ))
+    const hypotenuseLength = Math.sqrt(sumOfSquares);
+    outputDiv.innerText = "The length of hypotenuse is " + hypotenuseLength
+}
+
+
+hypotenuseBtn.addEventListener("click", checkHypotenuse)
